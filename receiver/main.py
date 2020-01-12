df1155c6badd574b89de2eff3881a940ff85d768
KevTango <kevin.tang2648@gmail.com>
2020-01-13 11:04:14 +1300

    rounded acceleration and temperature to 2 dp

--- receiver/main.py
+++ receiver/main.py
@@ -4,6 +4,7 @@ from serial import Serial
 import time
 import os
 import sys
+import math
 
 if sys.platform == 'esp32': # ESP32
   scl_pin = Pin(22, pull=Pin.PULL_UP, mode=Pin.OPEN_DRAIN)
@@ -17,7 +18,7 @@ else:
 i2c = I2C(-1, scl=scl_pin, sda=sda_pin)
 accelerometer = LIS3DH_I2C(i2c, int1=None)
 
-LIS3DH.range = RANGE_2_G # Setting range to 4G
+LIS3DH.range = RANGE_2_G # Setting range to 2G
 LIS3DH.datarate = DATARATE_1_HZ # Setting data rate to 1 Hz
 
 # Setting up communication
@@ -27,12 +28,31 @@ modbus_obj = Serial(uart_id)
 while True:
   # Print X,Y,Z acceleration values in m/s^2
   x, y, z = [value for value in accelerometer.acceleration]
-  print(x,y,z)
 
+  xRounded = round(x, 2)
+  yRounded = round(y, 2)
+  zRounded = round(z, 2)
+  print(xRounded, yRounded, zRounded)
+  
+  #print()
+  """
   # Read value of thermistor
-  temp = thermistor.read() / (2**12)
+  print(thermistor.read())
+  inverseADC = 4095 - thermistor.read()
+  print(inverseADC)
+  temp = inverseADC / (2**12 - 1)
+  temp -= 0.5
   temp *= 100
+  #temp *= 165
+  #temp -= 40
+  temp = round(temp, 2)
   print(temp)
+  print()
+  """
+  voltage = (thermistor.read() / 4095) * 3.3
+  temp = (voltage - 0.5) * 100
+  print(temp)
+  
   print()
   
   # Writing coil
@@ -40,10 +60,13 @@ while True:
   output_address=0x00
   output_value=0xFF00
 
-  return_flag = modbus_obj.write_single_coil(slave_addr, output_address, output_value)
-  output_flag = 'Success' if return_flag else 'Failure'
-  print('Writing single coil status: ' + output_flag)
-  print()
+  #return_flag = modbus_obj.write_single_coil(slave_addr, output_address, output_value)
+  #output_flag = 'Success' if return_flag else 'Failure'
+  #print('Writing single coil status: ' + output_flag)
+  #print()
   
   time.sleep(1)
+
+
+
 
