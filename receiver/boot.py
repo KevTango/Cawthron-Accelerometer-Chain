836d9c14f6c64f1e008da96458e4c1ec4810799f
KevTango <kevin.tang2648@gmail.com>
2020-01-29 16:59:53 +1300

    final logic finalised

--- receiver/boot.py
+++ receiver/boot.py
@@ -1,8 +1,1 @@
-# boot.py -- run on boot-up
-# can run arbitrary Python, but best to keep it minimal
-
-import machine
-import pyb
-#pyb.main('main.py') # main script to run after this one
-#pyb.usb_mode('VCP+MSC') # act as a serial and a storage device
-#pyb.usb_mode('VCP+HID') # act as a serial device and a mouse
+# boot.py -- run on boot-up# can run arbitrary Python, but best to keep it minimalimport pyb#pyb.main('main.py') # main script to run after this one#pyb.main('ds3231_port.py')#pyb.usb_mode('CDC+MSC') # act as a serial and a storage devicepyb.usb_mode('CDC+HID') # act as a serial device and a mouse
